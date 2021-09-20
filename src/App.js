36e1c104aed8eadffac24a03aafe06b35e02753e
kjo2354 <kjo2354@naver.com>
2021-09-21 02:09:01 +0900

    package.json index.html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import about from './pages/about';
 import contact from './pages/contact';
 import experience from './pages/experience';
@@ -8,11 +8,11 @@ import experience from './pages/experience';
 function App() {
   return (
     <div className="App">
-      <Router basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Route path="/" component={about} />
         <Route path="/contact" component={contact} />
         <Route path="/experience" component={experience} />
-      </Router>
+      </BrowserRouter>
     </div>
   );
 }
