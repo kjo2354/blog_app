56bda6276a9cf3c66d5d42b341415d8ccac9ecda
kjo2354 <kjo2354@naver.com>
2021-09-21 01:49:09 +0900

    Updates

--- src/pages/about.js
+++
@@ -1,9 +0,0 @@
-const about = function() {
-    return (
-      <>
-        <h1>about pages</h1>
-      </>
-    );
-  }
-
-  export default about;
--- src/pages/contact.js
+++
@@ -1,9 +0,0 @@
-function contact() {
-    return (
-      <>
-        <h1>contact pages</h1>
-      </>
-    );
-  }
-
-  export default contact;
